80b04ff01421fd410d8aefc26a65e8e478640373
Hemant Tumbare <hemant_tumbare@hotmail.com>
2023-08-02 11:36:26 +0530

    af

--- preparation/src/test/java/com/avaya/preparation/PreparationTest.java
+++ preparation/src/test/java/com/avaya/preparation/PreparationTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
