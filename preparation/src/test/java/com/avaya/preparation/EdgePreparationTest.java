5a674babd7045b69364e1faf57ddf1fffd6abb71
Hemant Tumbare <hemant_tumbare@hotmail.com>
2023-08-02 19:20:02 +0530

    re

--- preparation/src/test/java/com/avaya/preparation/EdgePreparationTest.java
+++ preparation/src/test/java/com/avaya/preparation/EdgePreparationTest.java
@@ -19,7 +19,7 @@ public class EdgePreparationTest {
 	  System.out.println("Launching Edge Browser...!!!");
 	  WebDriver driver=null;
 	  
-	  System.setProperty("webdriver.edge.driver","/usr/bin/msedgedriver");
+	  System.setProperty("webdriver.chrome.driver","/usr/bin/msedgedriver");
 	  
 	  
 	  EdgeOptions opt = new EdgeOptions();
